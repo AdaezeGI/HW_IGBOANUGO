78e030b7677c44d2e61055a99ace44edcfbcbc2e
Adaeze Igboanugo <ai5244a@american.edu>
2024-10-21 21:27:26 -0400

    Create 100 random normal numbers with mean 25 and standard deviation 1

---
+++ Igboanugo_hw.R
@@ -0,0 +1,6 @@
+# Generate 100 random normal numbers with mean = 25 and standard deviation = 1
+random_numbers <- rnorm(100, mean = 25, sd = 1)
+
+# Print the random numbers to verify
+print(random_numbers)
+
